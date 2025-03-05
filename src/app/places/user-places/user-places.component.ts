542c623872d2de0de23a31210776b637020276b7
Richard Burman <richard.burman@harperjames.co.uk>
2025-03-05 15:25:45 +0000

    Add fetching and error handling for user places in UserPlacesComponent

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
   standalone: true,
   templateUrl: './user-places.component.html',
-  styleUrl: './user-places.component.css',
+  styleUrl: './user-places.component.css', 
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+    isFetching = signal(false);
+    error = signal('');
+    private httpClient = inject(HttpClient);
+    private destoryRef = inject(DestroyRef);
+
+    ngOnInit() {
+      this.isFetching.set(true);
+      const subscription = this.httpClient
+        .get<{ places: Place[] }>('http://localhost:3000/user-places')
+        .pipe(
+          map((resData) => resData.places),
+          catchError((error) => {
+            console.log(error);
+            return throwError(() => new Error('Something went wrong fetching your favorite places!'));
+          })
+        )
+        .subscribe({
+          next: (places) => {
+            this.places.set(places);
+            // console.log(event.body?.places);
+          },
+          error: (error: Error) => {
+            this.error.set(error.message);
+          },
+          complete: () => {
+            this.isFetching.set(false);
+          },
+        });
+  
+      this.destoryRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
 }
