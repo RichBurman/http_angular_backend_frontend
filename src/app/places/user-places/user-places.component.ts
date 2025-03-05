5581201f98285e3db37fa31aa8f4a336d1dd031a
Richard Burman <richard.burman@harperjames.co.uk>
2025-03-05 16:27:17 +0000

    Refactor AvailablePlacesComponent and UserPlacesComponent to use PlacesService for data fetching and error handling

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -5,6 +5,7 @@ import { PlacesComponent } from '../places.component';
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -17,20 +18,13 @@ export class UserPlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
     isFetching = signal(false);
     error = signal('');
-    private httpClient = inject(HttpClient);
+   
+    private placesService = inject(PlacesService);
     private destoryRef = inject(DestroyRef);
 
     ngOnInit() {
       this.isFetching.set(true);
-      const subscription = this.httpClient
-        .get<{ places: Place[] }>('http://localhost:3000/user-places')
-        .pipe(
-          map((resData) => resData.places),
-          catchError((error) => {
-            console.log(error);
-            return throwError(() => new Error('Something went wrong fetching your favorite places!'));
-          })
-        )
+      const subscription = this.placesService.loadUserPlaces()
         .subscribe({
           next: (places) => {
             this.places.set(places);
